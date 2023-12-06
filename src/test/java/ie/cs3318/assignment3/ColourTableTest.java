46d86cc2e594a7ec9ea342eb9a4366ab150180a4
rabia <119311546@umail.ucc.ie>
2023-12-06 19:09:18 +0000

    Added add new colour statement to test file.

--- src/test/java/ie/cs3318/assignment3/ColourTableTest.java
+++ src/test/java/ie/cs3318/assignment3/ColourTableTest.java
@@ -1,7 +1,9 @@
 package ie.cs3318.assignment3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,11 +12,21 @@ public class ColourTableTest {
 //    @Test
     @ParameterizedTest
     @ValueSource(ints = {1024, 2, 1, 1025, 87})
-    public void createTable(int value){
+    public void createTable(int value) throws Exception {
         ColourTable colourTable = new ColourTable(value);
         assertEquals(value, colourTable.evaluate());
+
+        colourTable.addColour("FFFFFF");
 //        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-    }
+    };
+
+//    @ParameterizedTest
+//    @ValueSource( strings = {"a"})
+//    public void checkingException(int value) throws ParameterResolutionException, ArgumentConversionException, NumberFormatException {
+//        ColourTable colourTable = new ColourTable(value);
+//        assertEquals(value, colourTable.evaluate());
+////        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
+//    }
 
 //    public void addColour(){
 //        colourTable.addColour("${colour}");
