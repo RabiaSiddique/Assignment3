be9512f06019526930328500a45fef7f076ec74a
rabia <119311546@umail.ucc.ie>
2023-12-06 19:36:17 +0000

    Re-did evaluate method and created isPowerOfTwo method.

--- src/main/java/ie/cs3318/assignment3/ColourTable.java
+++ src/main/java/ie/cs3318/assignment3/ColourTable.java
@@ -7,7 +7,31 @@ public class ColourTable {
     }
 
     public Object evaluate() {
-        return null;
+//        return null;
+        //        String message = null;
+
+            if (isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1) {
+//                message = String.format("ColourTable can contain %s many values", noOfColoursInPalette);
+                return noOfColoursInPalette;
+            }
+            else{
+                return "Number is not within the valid range.";
+            }
+
+
+//        catch (Exception e) {
+////            message = String.format("ColourTable can not contain %s many values", noOfColoursInPalette);
+//            return e.getMessage();
+//        }
+
+//        return "Number is not valid";
+    }
+
+    static boolean isPowerOfTwo(int num)
+    {
+        /* First num in the below expression is
+        for the case when num is 0 */
+        return num != 0 && ((num & (num - 1)) == 0);
     }
 
     public void addColour(String newColour) {
