aee75503d71962235c36bfa212364c6cfe2dfe46
rabia <119311546@umail.ucc.ie>
2023-12-06 14:51:37 +0000

    Created Unit test to create colourtable.

---
+++ src/test/java/ie/cs3318/assignment3/ColourTableTest.java
@@ -0,0 +1,19 @@
+package ie.cs3318.assignment3;
+
+import org.junit.jupiter.api.Test;
+
+public class ColourTableTest {
+    @Test
+    public void createTable(){
+        ColourTable colourTable = new ColourTable(1024);
+        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
+    }
+
+//    public void addColour(){
+//        colourTable.addColour("${colour}");
+//        colourTable.set("colour", "FFFFFF");
+//    }
+
+
+
+}
