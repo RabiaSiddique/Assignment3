ef118506dc4a70402b22a4d19e8d1eb6a0643ee3
rabia <119311546@umail.ucc.ie>
2023-12-06 16:14:25 +0000

    Initialized parameter noOfColoursPalette in ColourTable.

--- src/main/java/ie/cs3318/assignment3/ColourTable.java
+++ src/main/java/ie/cs3318/assignment3/ColourTable.java
@@ -1,10 +1,13 @@
 package ie.cs3318.assignment3;
 
 public class ColourTable {
+    private final Object noOfColoursInPalette;
+
     public ColourTable(int noOfColoursInPalette) {
+        this.noOfColoursInPalette = noOfColoursInPalette;
     }
 
     public Object evaluate() {
-        return 1024;
+        return noOfColoursInPalette;
     }
 }
