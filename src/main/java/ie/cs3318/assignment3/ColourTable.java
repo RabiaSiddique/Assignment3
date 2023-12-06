e15fb963735e45c5addc3248528daf9400d0cec4
rabia <119311546@umail.ucc.ie>
2023-12-06 22:41:48 +0000

    Added syntax to make points clearer in test.

--- src/main/java/ie/cs3318/assignment3/ColourTable.java
+++ src/main/java/ie/cs3318/assignment3/ColourTable.java
@@ -13,29 +13,7 @@ public class ColourTable {
         this.noOfColoursInPalette = noOfColoursInPalette;
     }
 
-
-
-//    public Object evaluate() {
-////        return null;
-//        //        String message = null;
-//
-//        if (isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1) {
-////                message = String.format("ColourTable can contain %s many values", noOfColoursInPalette);
-//                return noOfColoursInPalette;
-//            }
-//            else{
-//                return "Number is not within the valid range.";
-//            }
-
-
-//        catch (Exception e) {
-////            message = String.format("ColourTable can not contain %s many values", noOfColoursInPalette);
-//            return e.getMessage();
-//        }
-
-//        return "Number is not valid";
-//    }
-
+    // Code from: https://www.geeksforgeeks.org/java-program-to-find-whether-a-no-is-power-of-two/
     static boolean isPowerOfTwo(int num)
     {
         /* First num in the below expression is
@@ -43,61 +21,39 @@ public class ColourTable {
         return num != 0 && ((num & (num - 1)) == 0);
     }
 
-
-        public HashSet createTable () {
-
-        if (correctSize() == true) {
-            HashSet<String> colourArray = new HashSet<>();
-            return colourArray;
-        } else {
-            return null;
-        }
-
+    public HashSet createTable ()
+    {
+    if (correctSize()) {
+        return new HashSet<String>();
+    } else {
+        return null;
+    }
     }
 
 
     public void addColour(HashSet table, String newColour){
-        if (table.size() < 1025 && isValidHexCode(newColour) == true)
-        table.add(newColour);
+        if (table.size() < 1025 && isValidHexCode(newColour)) {
+            table.add(newColour);
+        }
 
-        else if (isValidHexCode(newColour) == false) {
+        else if (!isValidHexCode(newColour)) {
             System.out.println("The colour code is invalid");
         }
 
     }
 
     public void getColours(HashSet table) {
-        Iterator<String> itr = table.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
+        for (String s : (Iterable<String>) table) {
+            System.out.println(s);
         }
     }
 
     public boolean correctSize() {
-        if (isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1) {
-//                message = String.format("ColourTable can contain %s many values", noOfColoursInPalette);
-            return true;
-        }
-        else{
-            return false;
-        }
+        return isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1;
     }
 
+    // Code from: https://www.geeksforgeeks.org/how-to-validate-hexadecimal-color-code-using-regular-expression/
     public boolean isValidHexCode(String newColour) {
-//        if (newColour.charAt(0) != '#')
-//            return false;
-//
-//        if (!(newColour.length() == 4 || newColour.length() == 7))
-//            return false;
-//
-//        for (int i = 1; i < newColour.length(); i++)
-//            if (!((newColour.charAt(i) >= '0' && newColour.charAt(i) <= 9)
-//                    || (newColour.charAt(i) >= 'a' && newColour.charAt(i) <= 'f')
-//                    || (newColour.charAt(i) >= 'A' || newColour.charAt(i) <= 'F')))
-//                return false;
-//
-//        return true;
-
         // Regex to check valid hexadecimal color code.
         String regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
 
@@ -120,63 +76,8 @@ public class ColourTable {
         return m.matches();
     }
 
-//    private int noOfColoursInPalette;
-//
-//    public ColourTable(int noOfColoursInPalette) {
-//        if (isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1) {
-//            this.noOfColoursInPalette = noOfColoursInPalette;
-//            String [] colourTableArray = new String[noOfColoursInPalette];
-//            colourTableArray.addColour
-//
-//        } else {
-//            System.out.println("The table size has to be to the power of 2, greater than 1 and less then 1025.");
-//        }
-//
-//    }
-//
-//
-//
-////    static boolean isPowerOfTwo(int num)
-////    {
-////        /* First num in the below expression is
-////        for the case when num is 0 */
-////        return num != 0 && ((num & (num - 1)) == 0);
-////    }
-//
-//    public Object evaluate() {
-////        String message = null;
-//
-//            if (isPowerOfTwo(noOfColoursInPalette) && noOfColoursInPalette < 1025 && noOfColoursInPalette > 1) {
-////                message = String.format("ColourTable can contain %s many values", noOfColoursInPalette);
-//                return noOfColoursInPalette;
-//            }
-//            else{
-//                return "Number is not within the valid range.";
-//            }
-//
-//
-////        catch (Exception e) {
-//////            message = String.format("ColourTable can not contain %s many values", noOfColoursInPalette);
-////            return e.getMessage();
-////        }
-//
-////        return "Number is not valid";
-//
-//    }
-//
-//    static boolean isPowerOfTwo(int num) {
-//        /* First num in the below expression is
-//        for the case when num is 0 */
-//        return num != 0 && ((num & (num - 1)) == 0);
-//    }
-//
-//    public void addColour(ColourTable colourtable, String newColour) {
-//         colourtable[colourtable.length] = newColour;
-//
-//
-//    }
-
-
-
+    public int getSize(HashSet colourArray) {
+        return colourArray.size();
     }
+}
 
--- src/test/java/ie/cs3318/assignment3/ColourTableTest.java
+++ src/test/java/ie/cs3318/assignment3/ColourTableTest.java
@@ -1,117 +1,67 @@
 package ie.cs3318.assignment3;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.opentest4j.AssertionFailedError;
 
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ColourTableTest {
-//    @Test
+    //    @Test
     @ParameterizedTest
     @ValueSource(ints = {1024, 2, 32})
-    public void createTable(int value){
+    public void createTable(int value) {
         ColourTable colourTable = new ColourTable(value);
-
-//        assertEquals(value, colourTable.evaluate());
 
         colourTable.correctSize();
 
         HashSet colourArray = colourTable.createTable();
 
-        colourTable.isValidHexCode("#9253cb");
-
+        System.out.println("Is Valid HexCode" + "- " + colourTable.isValidHexCode("#9253cb"));
 
         colourTable.addColour(colourArray, "#9253cb");
 
         colourTable.addColour(colourArray, "#5343cb");
 
         colourTable.getColours(colourArray);
-//        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-    };
+
+        System.out.println("Size" + "- " + colourTable.getSize(colourArray));
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {4})
-    public void inValidHexCode(int value){
+    public void inValidHexCode(int value) {
         ColourTable colourTable = new ColourTable(value);
-
-//        assertEquals(value, colourTable.evaluate());
 
         colourTable.correctSize();
 
         HashSet colourArray = colourTable.createTable();
 
-        colourTable.isValidHexCode("#zzzzzz");
+        System.out.println("Is Valid HexCode" + "- " + colourTable.isValidHexCode("#zzzzzz"));
 
         colourTable.addColour(colourArray, "#zzzzzz");
 
         colourTable.getColours(colourArray);
-//        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-    };
+        System.out.println("Size" + "- " + colourTable.getSize(colourArray));
 
-
-//    @ParameterizedTest
-//    @ValueSource(ints = {1754})
-//    public void tableNotFollowingSizeGuidelines (int value){
-//        ColourTable colourTable = new ColourTable(value);
-//
-////        assertEquals(value, colourTable.evaluate());
-//
-//        System.out.println(colourTable.correctSize());
-//
-//        HashSet colourArray = colourTable.createTable();
-//        colourTable.addColour(colourArray, "FFFFFF");
-//
-////        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-//    };
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 1754, 1})
-    public void tableNotFollowingSizeGuidelinesSoNullPointer(int value) {
-        assertThrows(NullPointerException.class,
-
-                ()->{
+    public void tableNotFollowingSizeGuidelinesSoNullPointer(int value)
+    {
+        assertThrows(NullPointerException.class, () ->
+        {
             ColourTable colourTable = new ColourTable(value);
 
             HashSet colourArray = colourTable.createTable();
-                    colourTable.addColour(colourArray, "FFFFFF");
+            colourTable.addColour(colourArray, "#9253cb");
 
-
-                    //do whatever you want to do here
-                    //ex : objectName.thisMethodShoulThrowNullPointerExceptionForNullParameter(null);
-                });
+        });
     }
+}
 
-
-//        assertEquals(value, colourTable.evaluate());
-
-//        System.out.println(colourTable.correctSize());
-//
-//        HashSet colourArray = colourTable.createTable();
-//        colourTable.addColour(colourArray, "FFFFFF");
-
-//        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-    };
-
-
-//    @ParameterizedTest
-//    @ValueSource( strings = {"a"})
-//    public void checkingException(int value) throws ParameterResolutionException, ArgumentConversionException, NumberFormatException {
-//        ColourTable colourTable = new ColourTable(value);
-//        assertEquals(value, colourTable.evaluate());
-////        assertEquals(1024, "[FFFFFF]", colourTable.evaluate());
-//    }
-
-//    public void addColour(){
-//        colourTable.addColour("${colour}");
-//        colourTable.set("colour", "FFFFFF");
-//    }
 
 
 
